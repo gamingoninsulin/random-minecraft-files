8fb3dc19b0618725d5931d2ca7280eae46b0e987
GamingOnInsulin <59486309+gamingoninsulin@users.noreply.github.com>
2024-10-02 17:48:22 +0200

    Update _start_server.bat

--- _start_server.bat
+++ _start_server.bat
@@ -7,16 +7,17 @@ title "Minecraft Server Console"
 
 
 rem Set the paper jar version to be downloaded from https://www.papermc.io/software/paper
-rem IMPORTANT!! USED "_" instead of "."
-set PAPER_VERSION=1_21
+rem IMPORTANT!! USED "_" instead of "." 
+rem also keep in mind to delete the current paperclip jar if you already have one and changed the version!
+set PAPER_VERSION=1_21_1
 
 
 rem Set the server JAR download URL
 set "jar_url=https://mineacademy.org/api/paper/%PAPER_VERSION%"
 
 
-rem Set the server JAR filename
-set "MC_SERVER_JAR=paperclip.jar"
+rem Set the server JAR filename with the desired version
+set "MC_SERVER_JAR=paperclip_%PAPER_VERSION%.jar"
 
 
 rem Set the Minecraft world name
